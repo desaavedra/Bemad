ff20078ca29010de9fa629353b4413412577e232
desaavedra <de.saavedra@uniandes.edu.co>
2019-10-06 15:35:09 -0500

    Primera Parte

--- app/src/main/java/tec/cvca/bemad/elemento/adapter/Adapter1.kt
+++ app/src/main/java/tec/cvca/bemad/elemento/adapter/Adapter1.kt
@@ -16,24 +16,25 @@ class Adapter1(private val context: Context, private val layout: Int,
     private val inflater: LayoutInflater
             = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View
-    {
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View    {
         val view = inflater.inflate(layout, p2, false)
-        val imgElemento = view.findViewById<ImageView>(R.id.imgImagen)
+
         val txtNombre = view.findViewById<TextView>(R.id.txvNombre)
+        val imgElemento = view.findViewById<ImageView>(R.id.imgImagen)
         val txvNegocio = view.findViewById<TextView>(R.id.txvTipoNegocio)
         val txvPuntaje = view.findViewById<TextView>(R.id.txvPuntaje)
 
 
         val elemento = getItem(p0) as Element
-
-        imgElemento.setImageResource(elemento.imagen)
-        txtNombre.setText(elemento.nombre)
-        txvNegocio.setText(elemento.tipoNegocio)
-        txvPuntaje.setText(elemento.puntaje)
+        txtNombre.text=elemento.nombre
+        txvNegocio.text = elemento.tipoNegocio
 
 
-        return view
+    txvPuntaje.text = elemento.puntaje.toString()
+    elemento.imagen
+    imgElemento.setImageResource(elemento.imagen)
+
+    return view
     }
 
     override fun getItem(p0: Int): Any {
